227f4ecb57215f83753b7a55cb07641afe71a4bb
vijay <vijaysubramanian23@gmail.com>
2017-10-12 10:28:11 +0530

    first commit

---
+++ webview/main/java/com/example/csh/webview/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.csh.webview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        WebView webView = (WebView)
+                findViewById(R.id.webview);
+        webView.loadUrl("file:///android_asset/dabblet.html");
+    }
+}
